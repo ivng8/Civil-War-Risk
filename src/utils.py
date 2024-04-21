6c90d36f3f67dd938cc374e1ae78e42383387666
Ivan Ng <147109497+ivng8@users.noreply.github.com>
2024-04-20 20:44:29 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,12 @@ def draw_multiline_text(
 def keyevent(attack: str, defend: str):
     if (attack.find('Virgina') + defend.find('Virgina') > -2):
         print("Battle of New Market Heights (1864): United States Colored Troops (USCT)," +
-              "primarily composed of African American soldiers, played a crucial role in the Union victory.")
+              "primarily composed of African American soldiers, played a crucial role in the Union victory." + 
+              "Battle of the Crater (1864): Although not primarily a battle involving racial minorities, " + 
+              "the involvement of the 4th Division of the United States Colored Troops in the failed assault " + 
+              "on Petersburg, Virginia, is notable. The Union plan to detonate explosives in a mine under " + 
+              "Confederate lines and exploit the resulting crater failed, resulting in heavy casualties, " + 
+              "including for African American soldiers.")
     elif (attack.find('South Carolina') + defend.find('South Carolina') > -1):
         print("Battle of Fort Wagner (1863): The 54th Massachusetts Infantry Regiment," +
               "comprised of African American soldiers, led the assault on Fort Wagner," + 
@@ -41,3 +46,20 @@ def keyevent(attack: str, defend: str):
     elif (attack.find('Arkansas') + defend.find('Arkansas') > -2):
         print("Battle of Pea Ridge (1862): Cherokee, Creek, and Seminole troops fought alongside" + 
               "Confederate forces in this engagement in Arkansas.")
+    elif (attack.find('Missouri') + defend.find('Missouri') > -2):
+        print("Battle of Island Mound (1862): This engagement in Missouri is notable for being one of the first times" + 
+              "African American soldiers fought in combat during the Civil War. The 1st Kansas Colored Volunteer" + 
+              "Infantry, primarily composed of African American soldiers, repelled Confederate guerrilla forces.")
+    elif (attack.find('Louisiana') + defend.find('Lousisiana') > -2):
+        print("Battle of Milliken's Bend (1863): African American troops, including the 9th and 11th Louisiana " + 
+              "Native Guard (later reorganized as the 1st and 2nd Louisiana Native Guard), played a significant " + 
+              "role in repelling a Confederate attack on a Union supply depot in Louisiana.")
+    elif (attack.find('Tennessee') + defend.find('Tennessee') > -2):
+        print("Battle of Fort Pillow (1864): This infamous battle in Tennessee saw a significant engagement" + 
+              " between Union forces, including African American troops, and Confederate forces led by General Nathan " + 
+              "Bedford Forrest. The Confederate troops massacred African American soldiers who had surrendered, " + 
+              "leading to outrage in the North and contributing to the escalation of the conflict.")
+    elif (attack.find('Florida') + defend.find('Florida') > -2):
+        print("Battle of Olustee (1864): This battle in Florida involved the 54th Massachusetts Infantry Regiment, " + 
+              "one of the most well-known African American units in the Union Army. Despite their valor, the Union forces, " + 
+              "including the 54th Massachusetts, were defeated by Confederate troops.")
